543d96d9028c7e2a0adc8e1a706c9c7b11a9c9c3
AstroNodes <105454859+Nodes-Astro@users.noreply.github.com>
2024-03-14 21:21:20 +0300

    Update avail.sh

--- avail.sh
+++ avail.sh
@@ -53,7 +53,7 @@ echo "DONE!"
 echo  -e "\e[0m" #console rengi resetlendi
 
 #Avail Light Node Installing
-printf  "Avail Light Client yükleniyor... (Bu aşama donanım özelliklerinize göre 20-30dk arası sürebilmektedir. Lütfen kurulum esnasında bağlantıyı kesmeyiniz)   \033[0;32m" && sleep 1
+printf  "Avail Light Client yükleniyor... (Bu aşama donanım özelliklerinize göre 20-30dk arası sürebilmektedir. Screende çalıştığı için açık tutmanıza gerek yoktur. CTRL + A + D ile çıkış yapabilirsiniz.)   \033[0;32m" && sleep 1
 git clone https://github.com/availproject/avail-light.git > /root/log.txt 2>&1
 cd avail-light
 git checkout v1.7.9 > /root/log.txt 2>&1
